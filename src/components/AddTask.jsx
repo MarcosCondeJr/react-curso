dbcd90d7aa2627747560052fa316d29c53834219
Marcos Conde <marcos14conde@hotmail.com>
2025-08-07 18:15:36 -0300

    feat: criando layout para adicionar as tasks

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,7 +1,22 @@
-function AddTask() {
+import { useState } from "react"
+
+function AddTask({onAddTaskSubmit}) {
+    const [title, setTitile] = useState("");
+    const [description, setDescription] = useState("");
+
     return (
-        <div>
-            <h1>AddTask</h1>
+        <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+            <input 
+                className="border border-slate-300 rounded-md outline-slate-400 px-4 py-2" 
+                type="text" 
+                placeholder="Digite o titulo da tarefa" 
+            />
+            <input 
+                className="border border-slate-300 rounded-md outline-slate-400 px-4 py-2" 
+                type="text" 
+                placeholder="Digite o titulo da tarefa" 
+            />
+            <button className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium">Adicionar</button>
         </div>
     )
 }
